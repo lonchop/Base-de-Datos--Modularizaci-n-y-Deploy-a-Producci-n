4a273b662193fe88e7c72b506a8685446057c47d
Orangel Jose Gonzalez <goranjose16@gmail.com>
2023-07-18 16:34:57 -0400

    Add runtime.txt

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
 from routers.user import user_router
+# import os
+# import uvicorn
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0",
+#                 port=int(os.environ.get("PORT", 8000)))
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
